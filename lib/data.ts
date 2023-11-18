5568ebadaa75a4aea4525e78f1a441a90675b816
izikod <terrakiuumm@gmail.com>
2023-11-18 01:42:23 +0100

    factorisation du code

---
+++ lib/data.ts
@@ -0,0 +1,36 @@
+const profileData = {
+  name: "Ahmet Sahin",
+  title: "Développeur Web Full Stack 😉",
+  description:
+    "Développeur Web Full Stack avec une solide expertise en JavaScript, TypeScript, Next.js, Node.js, PostgreSQL et une variété de technologies. Passionné par la résolution de problèmes et la création de solutions intuitives et performantes, je suis capable de m'adapter et monter en compétences très rapidement sur des sujets complexes et en terrain inconnu. J'adore particulièrement sortir de ma zone de confort.",
+};
+
+const skillsData = {
+  programmingLanguages: ["JavaScript", "TypeScript", "Bash", "Python"],
+  frameworksAndLibraries: ["Next.js", "React.js", "Google Apps Script"],
+  databases: ["MySQL", "PostgreSQL", "MongoDB"],
+};
+
+const experienceData = [
+  {
+    title: "Consultant Google Workspace | Numericoach",
+    date: "2022 - Maintenant",
+    description:
+      "Expert Google Apps Script, Google Cloud et outils Google Administrator. Prestation de services pour des clients de tous secteurs d'activité. Formations et développement sur mesure.",
+  },
+  {
+    title:
+      "Commercial Sédentaire en télétravail | Centre européen de formation",
+    date: "2021 - 2022",
+    description:
+      "Prospection de 55 leads par semaine, vente de contrat de formation, argumentaire et closing.",
+  },
+  {
+    title: "Téléconseiller Web | 1&1 IONOS",
+    date: "2017 - 2021",
+    description:
+      "Service client technique (appel entrant/sortant, mail et chat) pour les produits web de 1&1 IONOS (hébergement, nom de domaine, site web, etc.), aide sur la gestion de serveur, PHP, MySQL et quelques CMS de base.",
+  },
+];
+
+export { profileData, skillsData, experienceData };
